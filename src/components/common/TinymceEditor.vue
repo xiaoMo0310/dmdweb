c1ffdf8e34daf487058e82134b459e9449d79bae
ChenYanbing <49548849+JDChenYanbing@users.noreply.github.com>
2019-11-05 17:14:20 +0800

    修改富文本编辑器代码

--- src/components/common/TinymceEditor.vue
+++ src/components/common/TinymceEditor.vue
@@ -8,24 +8,21 @@
                width="60%"
                :lock-scroll="true"
                :close-on-click-modal="false">
-      <div class="image-upload-panel">
-        <div class="image-item"
-             v-for="(item,index) in imageList"
-             :key="index">
-          <img :src="item.url">
-          <span class="image-del el-icon-close"
-                @click="delImage(index)"></span>
-        </div>
-        <el-upload class="image-upload-btn"
-                   :action="api.uploadImage"
-                   name="upfile"
-                   accept="image/png, image/jpeg"
-                   with-credentials
-                   :show-file-list="false"
-                   :on-success="imageUploadSuccess"
-                   :before-upload="imageBeforeUpload">
-          <i class="el-icon-plus"></i>
+      <div>
+        <el-upload
+          action="#"
+          :data="dataObj"
+          list-type="picture"
+          :multiple="false" :show-file-list="showFileList"
+          accept="image/png,image/gif,image/jpg,image/jpeg"
+          :file-list="fileList"
+          :before-upload="beforeUpload"
+          :on-remove="handleRemove"
+          :on-preview="handlePreview">
+          <el-button size="small" type="primary" >点击上传</el-button>
+          <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过10MB</div>
         </el-upload>
+
       </div>
       <span slot="footer"
             class="dialog-footer">
@@ -48,6 +45,8 @@
   import 'tinymce/plugins/link'
   import 'tinymce/plugins/preview'
   import SingleUpload from '@/components/Upload/singleUpload'
+  import {uploadFile} from '@/api/upload'
+
   export default {
     components: {
       Editor
@@ -70,14 +69,46 @@
         default: 'undo redo |  formatselect fontselect fontsizeselect | bold italic forecolor backcolor  |  link imageUpload  table | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent|removeformat preview'
       }
     },
+    computed: {
+      imageUrl() {
+        return this.value;
+      },
+      imageName() {
+        if (this.value != null && this.value !== '') {
+          return this.value.substr(this.value.lastIndexOf("/") + 1);
+        } else {
+          return null;
+        }
+      },
+      fileList() {
+        return [{
+          name: this.imageName,
+          url: this.imageUrl
+        }]
+      },
+      showFileList: {
+        get: function () {
+          return this.value !== null && this.value !== ''&& this.value!==undefined;
+        },
+        set: function (newValue) {
+        }
+      }
+    },
     data () {
       return {
+        dataObj: {
+          fileName: '',
+          extName: '',
+          fileSize: '',
+          serverPath: ''
+        },
+        dialogVisible: false,
+        url: process.env.BASE_API + "/upload/file",
         loading: undefined,
         api: {
-          uploadImage: "/api/upload"
+          SingleUpload: SingleUpload
         },
         imageList: [],
-        dialogVisible: false,
         editor: null,
         init: {
           language_url: '/static/plugin/tinymce/langs/zh_CN.js',
@@ -108,6 +139,45 @@
       tinymce.init({})
     },
     methods: {
+      emitInput(val) {
+        this.$emit('input', val)
+      },
+      handleRemove(file, fileList) {
+        this.emitInput('');
+      },
+      handlePreview(file) {
+        this.dialogVisible = true;
+      },
+      beforeUpload(file) {
+        let isJPG = file.type === 'image/png' || file.type === 'image/gif' || file.type === 'image/jpg' || file.type === 'image/jpeg'
+        const isLt2M = file.size / 1024 / 1024 < 2
+        if(!isJPG) {
+          this.$notify.warning({
+            title: '警告',
+            message: '请上传格式为image/png, image/gif, image/jpg, image/jpeg的图片'
+          })
+        }
+        let size = file.size / 1024 / 1024 / 2
+        if(!isLt2M) {
+          this.$notify.warning({
+            title: '警告',
+            message: '图片大小必须小于2M'
+          })
+        }
+        //上传文件
+        let formData = new FormData();
+        formData.append('file', file);
+        let config = {
+          headers:{'Content-Type':'multipart/form-data'}
+        };
+        uploadFile(formData, config).then(response => {
+          this.showFileList = true;
+          this.fileList.pop();
+          this.fileList.push({name: file.name, url: response.result.serverPath});
+          this.emitInput(this.fileList[0].url);
+        });
+        return false;
+      },
       //删除图片
       delImage (index) {
         let imageList = this.imageList;
