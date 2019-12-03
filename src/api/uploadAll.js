1481747b2b16fe9a93763ff0529f1877775e2629
ChenYanbing <49548849+JDChenYanbing@users.noreply.github.com>
2019-12-03 18:18:06 +0800

    忽略

--- src/api/uploadAll.js
+++ src/api/uploadAll.js
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 //多张件上传
 export function uploadFileAll(data,config) {
   return request({
-    url:'/uploadFile/saveFile',
+    url:'/upload/saveFile',
     method:'post',
     data: data,
     config: config
